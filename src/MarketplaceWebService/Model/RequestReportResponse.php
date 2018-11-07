c1b91e81f95a3596f0055820651c9517c6b45146
tadashigondo <gondo@w-takumi.com>
2018-11-07 14:53:26 +0900

    Fix method name typo.

--- src/MarketplaceWebService/Model/RequestReportResponse.php
+++ src/MarketplaceWebService/Model/RequestReportResponse.php
@@ -184,7 +184,7 @@ class MarketplaceWebService_Model_RequestReportResponse extends MarketplaceWebSe
     {
         $xml = "";
         $xml .= "<RequestReportResponse xmlns=\"http://mws.amazonaws.com/doc/2009-01-01/\">";
-        $xml .= $this->_toXMLFragment();
+        $xml .= $this->toXMLFragment();
         $xml .= "</RequestReportResponse>";
         return $xml;
     }
